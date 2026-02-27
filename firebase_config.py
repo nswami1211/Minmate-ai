a4b3a045ecf4859af32cd94c0765e9801c18aa7e
GopiNath <nswami1211>
2026-02-28 04:34:19 +0530

    Fix Firebase config to use Streamlit secrets

--- firebase_config.py
+++ firebase_config.py
@@ -1,9 +1,23 @@
 import firebase_admin
 from firebase_admin import credentials, db, auth
-
-cred = credentials.Certificate("firebase_key.json")
+import streamlit as st
+import json
 
 if not firebase_admin._apps:
+    firebase_creds = {
+        "type": st.secrets["firebase"]["type"],
+        "project_id": st.secrets["firebase"]["project_id"],
+        "private_key_id": st.secrets["firebase"]["private_key_id"],
+        "private_key": st.secrets["firebase"]["private_key"].replace("\\n", "\n"),
+        "client_email": st.secrets["firebase"]["client_email"],
+        "client_id": st.secrets["firebase"]["client_id"],
+        "auth_uri": st.secrets["firebase"]["auth_uri"],
+        "token_uri": st.secrets["firebase"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"],
+        "universe_domain": st.secrets["firebase"]["universe_domain"],
+    }
+    cred = credentials.Certificate(firebase_creds)
     firebase_admin.initialize_app(cred, {
         "databaseURL": "https://mentalmadhilo-6900d-default-rtdb.firebaseio.com/"
     })
